6e394b2dc09349788dc16b3273d145df97f50ce2
subhash <subhash27042004@gmail.com>
2025-09-24 19:01:23 +0530

    updated server.js

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ app.post("/create-order", async (req, res) => {
             receipt: "order_rcptid_11"
         };
         const order = await razorpay.orders.create(options);
-            console.log("ORDER CREATED:", order);  // 👈 ye add karo
         res.json(order);
     } catch (err) {
         console.log(err);
@@ -60,54 +59,5 @@ app.post("/submit-form", upload.single("resume"), (req, res) => {
     res.status(500).json({ success: false, message: "Form submission failed" });
   }
 });
-
-// const express = require("express");
-// const multer = require("multer");
-// const fs = require("fs");
-// const path = require("path");
-// const cors = require("cors");
-
-// const app = express();
-// app.use(cors());
-
-// // Storage setup
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "uploads/");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + path.extname(file.originalname));
-//   },
-// });
-
-// const upload = multer({ storage });
-
-// // Application submit route
-// app.post("/submit-form", upload.single("resume"), (req, res) => {
-//   const application = {
-//     name: req.body.name,
-//     email: req.body.email,
-//     phone: req.body.phone,
-//     qualification: req.body.qualification,
-//     resume: req.file ? req.file.filename : null,
-//     submittedAt: new Date(),
-//   };
-
-//   // Save data in JSON file
-//   let filePath = "applications.json";
-//   let applications = [];
-
-//   if (fs.existsSync(filePath)) {
-//     applications = JSON.parse(fs.readFileSync(filePath));
-//   }
-
-//   applications.push(application);
-
-//   fs.writeFileSync(filePath, JSON.stringify(applications, null, 2));
-
-//   res.json({ success: true, message: "Application submitted!" });
-// });
-
-// app.listen(5000, () => console.log("Server running at http://localhost:5000"));
 
 
